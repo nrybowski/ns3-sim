9570cc39dc462e286d51ec1575233f4faf67cf62
nrybowski <nicolas.rybowski@uclouvain.be>
2022-01-24 13:37:43 +0100

    [bird/test] Fix bird config parsing + ms to ns conversion

--- myscripts/bird/ntf_parser.py
+++ myscripts/bird/ntf_parser.py
@@ -35,12 +35,16 @@ for line in data.split('\n'):
     split = line.split(' ')
     src, dst, metric, delay, begin, end = split if len(split) == 6 else split + [-1 for i in range(6-len(split))]
     key = (src, dst)
-    entry = {'f_metric': int(metric), 'delay': int(float(delay)*1000), 'r_metric': -1, 'begin': int(begin), 'end': int(end)}
+    entry = {'f_metric': int(metric), 'delay': round(float(delay)*1000000), 'r_metric': -1, 'begin': int(begin), 'end': int(end)}
 
     try:
         link = links[key]
-        if link[-1]['r_metric'] != -1:
+        if link[-1]['r_metric'] != -1 and link[-1]['f_metric'] != -1:
+            # link already parsed in both direction, we got a new link
             links[key].append(entry)
+        elif link[-1]['r_metric'] != -1:
+            # link already parsed in reverse direction, we set the forward metric
+            link[-1]['f_metric'] = entry['f_metric']
         else:
             print('#unexpected')
             exit(1)
@@ -48,14 +52,23 @@ for line in data.split('\n'):
         # no forward
         try:
             # got reverse
-            link = links[tuple([key[i-1] for i in range(len(key), 0, -1)])]
-            link[-1]['r_metric'] = int(metric)
+            reversed_key = tuple([key[i-1] for i in range(len(key), 0, -1)])
+            link = links[reversed_key]
+            if link[-1]['r_metric'] == -1:
+                # unset reverse of existing forward
+                link[-1]['r_metric'] = int(metric)
+            else:
+                # revsere and forward already set, got a new link
+                entry['r_metric'] = entry['f_metric']
+                entry['f_metric'] = -1
+                links[reversed_key].append(entry)
         except KeyError:
-            # no reverse => new link
+            # no forward nor reverse, we got a new link
             links[key] = [entry]
             for node in key:
                 if node not in nodes:
                     nodes.append(node)
+
 
 n_links = 0
 n_pairs = 0
@@ -93,10 +106,44 @@ else:
         for idx, node in enumerate(nodes):
             fd.write('%i %s\n' % (idx, node))
 
+from numpy import inf, zeros
+from scipy.sparse import csr_matrix
+
+mygraph = zeros((len(nodes), len(nodes)))
 id_src = mapping if len(mapping) > 0 else nodes
+fail = 0
+data = [' '.join(j.split(' ')[:-1] + [format(float(j.split(' ')[-1]), '.6f')]) for j in data.split('\n') if j != '' and  j[0] != '#']
 for (src, dst), l in links.items():
+    src_id = id_src.index(src)
+    dst_id = id_src.index(dst)
+
     for link in l:
-        print('%i %i %i %i %i %i %i' % (id_src.index(src), id_src.index(dst), link['f_metric'], link['r_metric'], link['delay'], link['begin'], link['end']))
+                # output for C++ wrapper
+        print('%i %i %i %i %i %i %i' % (src_id, dst_id, link['f_metric'], link['r_metric'], link['delay'], link['begin'], link['end']))
+
+        mygraph[src_id][dst_id] = link['f_metric']
+        mygraph[dst_id][src_id] = link['r_metric']
+
+        # 
+        for i in ['%s %s %i %.6f' % (src, dst, link['f_metric'], link['delay'] / 1000000), '%s %s %i %f' % (dst, src, link['r_metric'], link['delay'] / 1000000)]:
+            try:
+                data.remove(i)
+            except ValueError:
+                print('#Warning ', i)
+                print('#Warning ', link['delay'])
+                fail += 1
+
+print('#Failures %i' % fail)
+if len(data) != 0:
+    exit(1)
+
+from scipy.sparse.csgraph import connected_components
+mygraph = csr_matrix(mygraph)
+#with open('connected', 'w') as fd:
+n, labesl = connected_components(mygraph)
+if n != 1:
+    exit(1)
+
 
 #fd.flush()
 #fd.close()
--- myscripts/bird/test.py
+++ myscripts/bird/test.py
@@ -2,22 +2,57 @@
 
 import os
 import sys 
+from re import sub
+
+metrics = {}
 
 def get_metric(node_id, iface_id):
+    global metrics
+
+    try:
+        return metrics[node_id][iface_id]
+    except KeyError:
+        pass
+
+    # read <node_id> config file to get back the link metrics
     with open('files-%s/etc/bird.conf' % node_id) as fd:
         config = fd.read()
 
-    lines = config.split('\n')[:-1]
-    for lid, line in enumerate(lines):
-        if 'sim%s' % iface_id in line:
-            costs = [line for line in lines[lid:lid+5] if 'cost' in line]
-            if len(costs) > 1:
-                print('error')
+    last = -1
+    for line in config.split('\n'):
+        # skip empty line
+        if line == '': continue
+
+        # remove tabulations and split line by spaces
+        line = [i for i in sub('\t', '', line).split(' ') if i != '']
+        if line[0] == 'interface' and line[1][:4] == '"sim':
+            # 'sim*' interface configuration block
+            current_iface_id = line[1][4:-1]
+            if last == -1:
+                # parse new interface
+                last = current_iface_id
+            else:
+                # error, we try to parse a new interface while the previous one is not completed
+                print('ERROR: try to parse %s while %s is not finished' % (current_iface_id, last))
                 exit(1)
-            metric = costs[0].split(' ')[-1][:-1]
-    return metric
+        elif line[0] == 'cost' and len(line) == 2:
+            # got the cost of the link seen by an interface
+            metric = line[1][:-1] 
+            if last != -1:
+                try:
+                    metrics[node_id][last] = metric
+                except KeyError:
+                    metrics[node_id] = {last: metric}
+                last = -1
+            else:
+                print('ERROR: got a metric not associated to an interface')
+                exit(1)
+
+    return metrics[node_id][iface_id]
 
 def nodemap(filename: str) -> dict:
+    """ Collect nodemap in dict
+    """
     ret = {}
     with open(filename, 'r') as fd:
         data = fd.read()
@@ -37,18 +72,26 @@ if __name__ == '__main__':
     generated_topo = []
     for line in csv:
         n1, i1, n2, i2, delay = line.split(',')
-        generated_topo.append('%s %s %s %s' % (nodes[n1], nodes[n2], get_metric(n1, i1), delay))
+        msg = '%s %s %s %s' % (nodes[n1], nodes[n2], get_metric(n1, i1), delay)
+
+        generated_topo.append(msg)
 
     with open(sys.argv[3], 'r') as fd:
         ntf = fd.read()
-    for line in ntf.split('\n')[:-1]:
-        if line[0] == '#':
+    ntf = ntf.split('\n')
+    for line in ntf:
+        if line == '' or line[0] == '#':
             continue
-
+        splitted = line.split(' ')
+        new_line = ' '.join(splitted[:-1] + [format(float(splitted[-1]), '.6f')])
         try:
-            generated_topo.remove(line)
+            generated_topo.remove(new_line)
         except ValueError:
-            print('ERROR: <%s> link not found in generated topo' % line)
+            print('ERROR: <%s> link not found in generated topo' % new_line)
+            for item in generated_topo:
+                if ' '.join(line.split(' ')[:2]) in item:
+                    print(item)
+
             exit(1)
 
     if (len(generated_topo) > 0):
