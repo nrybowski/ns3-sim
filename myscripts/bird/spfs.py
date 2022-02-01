0039bbc2d4f60011fd73094da8674db4ac229441
nrybowski <nicolas.rybowski@uclouvain.be>
2022-02-01 22:07:10 +0000

    [bird/spfs] Full mesh with new addresses

--- myscripts/bird/spfs.py
+++ myscripts/bird/spfs.py
@@ -4,6 +4,21 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import dijkstra
 import numpy as np
 
+def gen_addresses(n: int) -> list:
+    high = 0
+    low = 1
+
+    addresses = []
+
+    for i in range(n):
+            if low > 250:
+                    high += 1
+                    low = 1
+            addresses.append('10.1.%i.%i' % (high, low))
+            low += 1
+
+    return addresses
+
 if len(sys.argv) < 2:
     print("Usage:")
     exit(1)
@@ -30,6 +45,7 @@ for line in data:
 
 #print(nodes)
 n = len(nodes)
+addr = gen_addresses(n)
 graph = []
 for idx, node in enumerate(nodes):
     graph.append([links[node][nodes[i]] if nodes[i] in links[node] else np.inf for i in range(0, n)])
@@ -41,7 +57,8 @@ print(graph)
 _, predecessors = dijkstra(csgraph=csr_matrix(np.array(graph)), return_predecessors=True)
 print(predecessors)
 
-leaves = [['10.0.1.%i' % (i+1) for i in range(len(nodes)) if i not in pred] for pred in predecessors]
+#leaves = [[addr[i] for i in range(len(nodes)) if i not in pred] for pred in predecessors]
+leaves = [[addr[i] for i in range(n) if i != j] for j in range(n)]
 #leaves = [['10.0.1.%i' % (i+1) for i in range(len(nodes))] for j in range(len(nodes))]
 
 with open('udp.ping', 'w') as fd:
