23e50ee2fab552e9c2a4b6c27bd339f53305642b
nrybowski <nicolas.rybowski@uclouvain.be>
2022-02-01 22:02:18 +0000

    [containers] Dump data in shm

--- containers/script.sh
+++ containers/script.sh
@@ -4,8 +4,8 @@ info () {
     echo -e "[INFO][container] $*"
 }
 
-cp myscripts/bird/*.py .
-cp /data/inputs/* .
+cp myscripts/bird/*.py /dev/shm
+cp /data/inputs/* /dev/shm
 
 info "Re-compiling bird."
 CURDIR="${PWD}"
@@ -29,17 +29,17 @@ rm /data/my_exe/udp_client /data/my_exe/udp_server
 make -C /data/my_exe -j $(nproc)
 
 info "Running NS3."
-time ./waf --disable-tests --disable-examples --run dce-ntf-bird --command-template "%s ${1}"
+time ./waf --cwd=/dev/shm --disable-tests --disable-examples --run dce-ntf-bird --command-template "%s ${1}"
 
-info "Moving pcap traces."
-if [[ ! -d /data/output/traces ]]; then mkdir /data/output/traces; fi
-mv -f *.pcap *.map /data/output/traces > /dev/null 2>&1
+#info "Moving pcap traces."
+#if [[ ! -d /data/output/traces ]]; then mkdir /data/output/traces; fi
+#mv -f *.pcap *.map /data/output/traces > /dev/null 2>&1
 
-info "Moving node files."
-if [[ ! -d /data/output/files ]]; then mkdir /data/output/files; else rm -rf /data/output/files/*; fi
-mv -f files-* /data/output/files > /dev/null 2>&1
+#info "Moving node files."
+#if [[ ! -d /data/output/files ]]; then mkdir /data/output/files; else rm -rf /data/output/files/*; fi
+#mv -f files-* /data/output/files > /dev/null 2>&1
 
 #info ""
-mv -f *.check *.xml /data/output > /dev/null 2>&1
+#mv -f *.check *.xml /data/output > /dev/null 2>&1
 
 info "Leaving container."
